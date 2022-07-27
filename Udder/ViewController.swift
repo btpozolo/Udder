396f50115d984c03899f8ffe1f2637b5fe1c6d3b
Blake Pozolo <blake.pozolo@gmail.com>
2022-07-26 21:56:04 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Blake Pozolo on 7/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
